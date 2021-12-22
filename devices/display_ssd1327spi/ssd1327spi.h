9332d831d47b1acd38e926229fd2a3c4aab20491
Dawid Kurzyniec <dawidk@google.com>
2021-12-22 01:21:26 +0100

    Added framebuffer.

--- devices/display_ssd1327spi/ssd1327spi.h
+++ devices/display_ssd1327spi/ssd1327spi.h
@@ -1,13 +1,16 @@
 #pragma once
 
 #include "roo_testing/buses/spi/fake_spi.h"
+#include "roo_testing/devices/roo_display/fltk_framebuffer.h"
 
 class FakeSsd1327Spi : public SimpleFakeSpiDevice {
  public:
-  FakeSsd1327Spi(int cs, int dc, int rst)
+  FakeSsd1327Spi(int cs, int dc, int rst, int magnification = 1,
+                 Framebuffer::Rotation rotation = Framebuffer::kRotationNone)
       : SimpleFakeSpiDevice(cs),
         pinDC_(new FakeGpioPin()),
-        pinRST_(new FakeGpioPin()) {
+        pinRST_(new FakeGpioPin()),
+        framebuffer_(128, 128, magnification, rotation) {
     getGpioInterface()->attach(dc, pinDC_);
     getGpioInterface()->attach(rst, pinRST_);
   }
@@ -18,4 +21,6 @@ class FakeSsd1327Spi : public SimpleFakeSpiDevice {
  private:
   FakeGpioPin* pinDC_;
   FakeGpioPin* pinRST_;
+
+  Framebuffer framebuffer_;
 };

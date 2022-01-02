100ce5774c8fd5ff8f663c0b50f5eb38134e2b49
Dawid Kurzyniec <dawidk@google.com>
2022-01-03 00:59:02 +0100

    Added the possiblity to 'decalibrate' the simulated XPT2046 touch, to closer match real behavior.

--- devices/touch_xpt2046/touch_xpt2046spi.h
+++ devices/touch_xpt2046/touch_xpt2046spi.h
@@ -5,12 +5,50 @@
 
 class FakeXpt2046Spi : public SimpleFakeSpiDevice {
  public:
+  struct Calibration {
+    Calibration(int16_t xMin, int16_t yMin, int16_t xMax, int16_t yMax)
+        : xMin(xMin),
+          yMin(yMin),
+          xMax(xMax),
+          yMax(yMax),
+          right_to_left(false),
+          bottom_to_top(false),
+          swap_xy(false) {}
+
+    Calibration() : Calibration(0, 0, 4095, 4095) {}
+
+    int16_t calibrate(int16_t& x, int16_t& y) {
+      if (bottom_to_top) {
+        y = 4095 - y;
+      }
+      if (right_to_left) {
+        x = 4095 - x;
+      }
+      if (swap_xy) {
+        std::swap(x, y);
+      }
+      x = xMin + (x * (xMax - xMin + 1) + 4095) / 4096;
+      y = yMin + (y * (yMax - yMin + 1) + 4095) / 4096;
+    }
+
+    int16_t xMin;
+    int16_t yMin;
+    int16_t xMax;
+    int16_t yMax;
+    bool right_to_left;
+    bool bottom_to_top;
+    bool swap_xy;
+  };
+
   FakeXpt2046Spi(int cs, Viewport& viewport,
+                 Calibration calibration = Calibration(),
                  const std::string& name = "touch_XPT2046")
       : SimpleFakeSpiDevice(name, cs),
         viewport_(viewport),
+        calibration_(calibration),
         conversion_requested_(false),
-        conversion_bytes_returned_(0) {}
+        conversion_bytes_returned_(0) {
+  }
 
   void transfer(const FakeSpiInterface& spi, uint8_t* buf,
                 uint16_t bit_count) override {
@@ -37,15 +75,22 @@ class FakeXpt2046Spi : public SimpleFakeSpiDevice {
     int16_t x;
     int16_t y;
     bool clicked = viewport_.isMouseClicked(&x, &y);
+    if (clicked) {
+      x = ((4096LL * x) / viewport_.width());
+      y = ((4096LL * y) / viewport_.height());
+      calibration_.calibrate(x, y);
+      x = 4095 - x;
+      y = 4095 - y;
+    }
     switch (val & 0xF0) {
       case 0xD0: {
         conversion_requested_ = true;
-        conversion_ = clicked ? ((4096LL * x) / viewport_.width()) << 3 : 0;
+        conversion_ = clicked ? x << 3 : 0;
         break;
       }
       case 0x90: {
         conversion_requested_ = true;
-        conversion_ = clicked ? ((4096LL * y) / viewport_.height()) << 3 : 0;
+        conversion_ = clicked ? y << 3 : 0;
         break;
       }
       case 0xB0: {
@@ -63,6 +108,7 @@ class FakeXpt2046Spi : public SimpleFakeSpiDevice {
   }
 
   Viewport& viewport_;
+  Calibration calibration_;
   bool conversion_requested_;
   uint8_t conversion_bytes_returned_;
   uint16_t conversion_;

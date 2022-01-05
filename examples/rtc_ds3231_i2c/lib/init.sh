0e5c7ae86c03d53a0a44330fe1d2ce2b61f7adb4
Dawid Kurzyniec <dawidk@google.com>
2022-01-05 23:38:05 +0100

    Added an RTC DS3231 example.

---
+++ examples/rtc_ds3231_i2c/lib/init.sh
@@ -0,0 +1,1 @@
+cd `dirname $0`; git clone https://github.com/jarzebski/Arduino-DS3231
---
+++ examples/rtc_ds3231_i2c/src/main.cpp
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+
+#ifdef ROO_TESTING
+
+#include "roo_testing/devices/rtc_ds3231/ds3231.h"
+
+struct Emulator {
+  FakeDs3231 rtc;
+  Emulator() {
+    // NOTE: the I2C emulation is implemented at the interface level, not
+    // hardware level (for now) - i.e. the devices get attached directly to the
+    // ESP32 I2C bus, and not indirectly via pins.
+    FakeEsp32().i2c[0].attach(rtc);
+  }
+} emulator;
+
+#endif
+
+#include "DS3231.h"
+
+DS3231 rtc;
+
+void setup() {
+  Serial.begin(9600);
+  rtc.begin();
+
+  // Setting the clock to some arbitrary time.
+  rtc.setDateTime(2022, 02, 21, 13, 55, 40);
+}
+
+void loop(void) {
+  auto now = rtc.getDateTime();
+  // NOTE: the dateFormat functions have a bug: they return a pointer to
+  // an object that has been already destroyed.
+  // Serial.println(rtc.dateFormat("d-m-Y H:i:s", now));
+
+  Serial.printf("%04d-%02d-%02d %02d:%02d:%02d\n", now.year, now.month, now.day,
+                now.hour, now.minute, now.second);
+
+  delay(999);
+}

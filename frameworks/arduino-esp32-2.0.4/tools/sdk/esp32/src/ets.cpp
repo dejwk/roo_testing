3d9d55094ebc4f940d942dec463b69bcfcf4fdc5
Dawid Kurzyniec <dawidk@google.com>
2022-11-04 19:59:18 +0100

    rom printing

--- frameworks/arduino-esp32-2.0.4/tools/sdk/esp32/src/ets.cpp
+++ frameworks/arduino-esp32-2.0.4/tools/sdk/esp32/src/ets.cpp
@@ -1,3 +1,6 @@
+#include <stdarg.h> /* va_list, va_start, va_arg, va_end */
+#include <stdio.h>  /* printf */
+
 #include "rom/ets_sys.h"
 
 extern "C" {
@@ -6,6 +9,12 @@ void ets_delay_us(uint32_t us) {}
 
 void ets_install_putc1(void (*p)(char c)) {}
 
-int ets_printf(const char *fmt, ...) { return 0; }
+int ets_printf(const char *fmt, ...) {
+  va_list arglist;
+  va_start(arglist, fmt);
+  vprintf(fmt, arglist);
+  va_end(arglist);
+  return 0;
+}
 
 }  // extern "C"

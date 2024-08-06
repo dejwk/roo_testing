e238c02c0cc0f2f94a58a15f11ffcb17d79cb137
dejwk <dawid.kurzyniec@gmail.com>
2024-08-06 17:10:52 +0200

    Fixing PCF8574 to stop returning random readings on write ports that have not been written to just yet.

--- devices/port_expander/pcf8574/pcf8574.cpp
+++ devices/port_expander/pcf8574/pcf8574.cpp
@@ -9,7 +9,7 @@
 using namespace roo_testing_transducers;
 
 FakeI2cPcf8574::FakeI2cPcf8574(uint8_t addr)
-    : FakeI2cDevice("PCF8574", 0x20 + addr), gpio(8) {
+    : FakeI2cDevice("PCF8574", 0x20 + addr), gpio(8), initialized_(false) {
   CHECK_LT(addr, 8);
 }
 
@@ -33,6 +33,17 @@ FakeI2cDevice::Result FakeI2cPcf8574::read(uint8_t *buf, uint16_t size,
                                            bool sendStop,
                                            uint16_t timeOutMillis) {
   CHECK_EQ(1, size);
+  if (!initialized_) {
+    gpio.get(0).digitalWrite(kDigitalHigh);
+    gpio.get(1).digitalWrite(kDigitalHigh);
+    gpio.get(2).digitalWrite(kDigitalHigh);
+    gpio.get(3).digitalWrite(kDigitalHigh);
+    gpio.get(4).digitalWrite(kDigitalHigh);
+    gpio.get(5).digitalWrite(kDigitalHigh);
+    gpio.get(6).digitalWrite(kDigitalHigh);
+    gpio.get(7).digitalWrite(kDigitalHigh);
+    initialized_ = true;
+  }
   buf[0] = (gpio.get(0).digitalRead() << 0) | (gpio.get(1).digitalRead() << 1) |
            (gpio.get(2).digitalRead() << 2) | (gpio.get(3).digitalRead() << 3) |
            (gpio.get(4).digitalRead() << 4) | (gpio.get(5).digitalRead() << 5) |

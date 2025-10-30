14d1b3da40a4cebc8429de6faa85fd827e656748
Dawid Kurzyniec <dawid.kurzyniec@gmail.com>
2025-10-30 23:20:52 +0100

    Fix: locks should be no-op if taken from outside of a task (when a scheduler is not yet running; generally in static initializers - i.e. we can assume mutual exclusion).

--- roo_testing/sys/mutex.cpp
+++ roo_testing/sys/mutex.cpp
@@ -8,12 +8,28 @@ namespace roo_testing {
 mutex::mutex() noexcept { xSemaphoreCreateMutexStatic(&mutex_); }
 
 void mutex::lock() {
+  if (xTaskGetCurrentTaskHandle() == nullptr) {
+    // Scheduler not yet running; no-op.
+    return;
+  }
   xSemaphoreTake((SemaphoreHandle_t)&mutex_, portMAX_DELAY);
 }
 
-bool mutex::try_lock() { return xSemaphoreTake((SemaphoreHandle_t)&mutex_, 0); }
+bool mutex::try_lock() {
+  if (xTaskGetCurrentTaskHandle() == nullptr) {
+    // Scheduler not yet running; no-op.
+    return true;
+  }
+  return xSemaphoreTake((SemaphoreHandle_t)&mutex_, 0);
+}
 
-void mutex::unlock() { xSemaphoreGive((SemaphoreHandle_t)&mutex_); }
+void mutex::unlock() {
+  if (xTaskGetCurrentTaskHandle() == nullptr) {
+    // Scheduler not yet running; no-op.
+    return;
+  }
+  xSemaphoreGive((SemaphoreHandle_t)&mutex_);
+}
 
 namespace internal {
 

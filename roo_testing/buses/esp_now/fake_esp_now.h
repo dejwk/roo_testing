2566f69d2df64779e2ea65e26d8099f574e21674
Dawid Kurzyniec <dawid.kurzyniec@gmail.com>
2025-10-30 20:27:17 +0100

    Refactored the simulated esp-now.

--- roo_testing/buses/esp_now/fake_esp_now.h
+++ roo_testing/buses/esp_now/fake_esp_now.h
@@ -1,16 +1,8 @@
 #pragma once
 
-#include <condition_variable>
-#include <cstddef>
+#include <cstdint>
 #include <functional>
-#include <initializer_list>
-#include <map>
-#include <memory>
-#include <queue>
-
-#include "roo_testing/sys/mutex.h"
-#include "roo_testing/sys/thread.h"
-#include "roo_testing/sys/condition_variable.h"
+#include <utility>
 
 class FakeEspNowDevice {
  public:
@@ -26,10 +18,11 @@ class FakeEspNowDevice {
   uint64_t mac_addr() const { return addr_; }
 
   void respond(const void* data, size_t len) { 
-    if (receive_cb_ != nullptr) receive_cb_(data, len); }
+    if (receive_cb_ != nullptr) receive_cb_(data, len);
+  }
 
  private:
-  friend class FakeEspNowInterface;
+  friend class EspNowInterface;
 
   void set_receive_cb(ReceiveCb receive_cb) {
     receive_cb_ = std::move(receive_cb);
@@ -37,53 +30,4 @@ class FakeEspNowDevice {
 
   uint64_t addr_;
   ReceiveCb receive_cb_;
-};
-
-class FakeEspNowInterface {
- public:
-  using OnSentCb = std::function<void(const uint8_t*, bool)>;
-  using OnRecvCb = std::function<void(const uint8_t*, const uint8_t*, size_t)>;
-
-  struct Packet {
-    uint64_t peer_mac;
-    int size;
-    std::unique_ptr<uint8_t[]> payload;
-  };
-
-  FakeEspNowInterface();
-
-  FakeEspNowInterface(
-      std::vector<std::pair<uint64_t, std::unique_ptr<FakeEspNowDevice>>>
-          devices);
-
-  ~FakeEspNowInterface();
-
-  void start();
-
-  //   void addDevice(uint64_t mac, FakeEspNowDevice* device) {
-  //     addDevice(mac, std::unique_ptr<FakeEspNowDevice>(device));
-  //   }
-
-  void attachDevice(FakeEspNowDevice& device);
-
-  bool send(const uint8_t* dest_mac, const void* data, size_t len);
-
-  void setSendCb(OnSentCb send_cb);
-  void setRecvCb(OnRecvCb recv_cb);
-
- private:
-  friend void process_inbox_fn(FakeEspNowInterface*);
-
-  void incoming(uint64_t peer, const void* data, size_t len);
-
-  void processInbox();
-
-  std::map<uint64_t, FakeEspNowDevice*> devices_;
-  std::queue<Packet> inbox_;
-  roo_testing::mutex mutex_;
-  roo_testing::condition_variable nonempty_;
-  OnSentCb on_sent_cb_;
-  OnRecvCb on_recv_cb_;
-
-  roo_testing::thread inbox_processor_;
 };
